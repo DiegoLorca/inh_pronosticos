03e87e097e2de963421f37be2991cc9033c780d1
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2020-04-30 20:18:00 -0400

    agregado mapa

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')
 
-function iniciarMap(){
-  var coord = { lat: -33.0247691, lng: -71.634644}
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: coord
-  });
+var mymap = L.map('map').setView([51.505, -0.09], 13);
 
-  var marker = new google.maps.marker({
-    position: coord,
-    map: map
-  });
-}
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGllZ29sY2giLCJhIjoiY2s5bmVhc2drMDFmMjNnbndxeTlxdG4wdCJ9.RLR93Zeo1YMGj0PXVm8o5w'
+}).addTo(mymap);
